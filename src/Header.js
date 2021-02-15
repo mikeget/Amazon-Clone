507b9bc7b5bdd86544394e3f71c46a6483ac91df
Michael Getachew <mikegeta79@gmail.com>
2021-02-15 04:02:30 -0500

    Subtotal

--- src/Header.js
+++ src/Header.js
@@ -17,16 +17,16 @@ import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
         <div className="header__nav">
             <div className="header__option">
             <span className="header__optionLineOne">Hellow Guest</span>
-            <span className="header__optionLineOne"> Sign In</span>
+            <span className="header__optionLineTwo"> Sign In</span>
             </div>
 
             <div className="header__option">
             <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineOne"> & Order</span>
+            <span className="header__optionLineTwo"> & Order</span>
             </div>
             <div className="header__option">
             <span className="header__optionLineOne">Your</span>
-            <span className="header__optionLineOne"> Prime</span>
+            <span className="header__optionLineTwo"> Prime</span>
                 </div>
                 <div className="header__optionBasket">
                     <ShoppingBasketIcon />
